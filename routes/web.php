1f626bb211aa9f3e3c763e1051aad594c579dbba
Fmelchor Nikken <fmelchor@nikkenlatam.com>
2024-03-13 19:56:32 -0600

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,3 +13,13 @@ Route::get('NikkenCMS/{view}', 'NikkenCMS\NikkenCMSController@getViwe');
 Route::get('NikkenCMSpro/getActions', 'NikkenCMS\NikkenCMSController@getActions');
 Route::get('NikkenCMS/encripytarPass/{pass}', 'NikkenCMS\NikkenCMSController@aes_sap_encrypt');
 Route::match(['get','post'],'addMicroSitio', 'NikkenCMS\NikkenCMSController@addMicroSitio')->name('addMicroSitio');
+
+/*
+
+Calle - nuevo campo de texto abierto a 250 caracteres
+Nombre de conjunto resindecial (Condominio, Edificio, etc.) - nuevo campo de texto abierto a 250 caracteres
+NO. Casa/apartamento - reemplaza a referencia
+
+codigo tel RD: +1 849-682-2000
+
+*/
